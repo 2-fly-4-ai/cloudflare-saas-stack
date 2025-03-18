9e4f720fe1896c9e1a7e268d48da3c4fe1211c28
2-fly-4-ai <farleythecoder@gmail.com>
2025-03-19 09:59:09 +1200

    testing opennext

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,7 +1,5 @@
 import type { NextRequest } from 'next/server'
-import { getRequestContext } from '@cloudflare/next-on-pages'
 
-export const runtime = 'edge'
 
 export async function GET(request: NextRequest) {
   let responseText = 'Hello World'
