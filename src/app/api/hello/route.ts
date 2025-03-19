a75a2cbd30ac874f27fdb6da40800a203ced25d1
2-fly-4-ai <farleythecoder@gmail.com>
2025-03-20 05:22:06 +1200

    fixing cloudflare auth

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,5 +1,7 @@
 import type { NextRequest } from 'next/server'
+import { getRequestContext } from '@cloudflare/next-on-pages'
 
+export const runtime = 'edge'
 
 export async function GET(request: NextRequest) {
   let responseText = 'Hello World'
